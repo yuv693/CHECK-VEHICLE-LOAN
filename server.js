4742456d5b0e049960113745938bac2bdc3d503d
check_vehicle_loan <malekyunus44@gmail.com>
2026-09-16 21:44:05 +0530

    Change API key in server.js
    Updated API key for vehicle information service.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const vehicleCache = new Map();
 const requestTracker = new Map();
 
 // Your API Key and correct endpoint configuration
-const API_KEY = '9e90b931e9mshbfd1742e256fe30p1e9cdajsn13d954df9c97';
+const API_KEY = '585ac7aa73msh97474a983a488d1p1fb0cdjsnc0e1c4f75737';
 const API_HOST = 'rto-vehicle-information-verification-india.p.rapidapi.com';
 const API_URL = 'https://rto-vehicle-information-verification-india.p.rapidapi.com/api/v1/rc';
 
