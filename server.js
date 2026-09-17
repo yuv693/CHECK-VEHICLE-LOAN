c622fafc9d59bb36b9e15273173be15d3886fe5b
check_vehicle_loan <malekyunus44@gmail.com>
2026-09-17 15:01:52 +0530

    Fix duplicate 'const' keyword in API_URL declaration

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const requestTracker = new Map();
 // Your API Key and correct endpoint configuration
 const API_KEY = 'bab79548femsh66e05a7c56ab71bp1e6ac7jsn4a0dadaa39df';
 const API_HOST = 'abhiyanpa7.p.rapidapi.com';
-const const API_URL = 'https://abhiyanpa7.p.rapidapi.com/address';
+const API_URL = 'https://abhiyanpa7.p.rapidapi.com/address';
 const htmlPage = `
 <!DOCTYPE html>
 <html lang="en">
